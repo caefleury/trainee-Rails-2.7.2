2936884c823d5b7788ceb405868f184a76e3df20
caefleury <caefleury15@gmail.com>
2022-05-01 23:39:33 -0300

    adicionando o user

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::UsersController < ApplicationController
+    def login
+        user = User.find_by!(email: params[:email])
+        if user.valid_password?(params[:password])
+            render json: user, status: :ok
+        else
+            head(:unauthorized)
+        end
+    rescue StandardError => e
+        render json: {message: e.message},status: :not_found
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, skip: :all
+  devise_for :admins, skip: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do
+
+      scope 'users' do
+        get 'login', to: 'users#login'
+      end
+
       scope 'products' do
         get 'index', to: 'products#index'
         get 'show/:id', to: 'products#show'
